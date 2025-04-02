75349907407ba7cb6a6fe7915f457e645bd52814
Alex Espejo <aespejo102@gmail.com>
2025-04-02 11:39:54 -0700

    added files

---
+++ src/components/Icons/Icon.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+ export let name: string;
+ export let size: number = 24;
+
+ // The public directory is typically served at the root path
+ const src = `/icons/${name}.svg`;
+</script>
+
+<img {src} alt={name} width={size} height={size} />
+
+<style>
+ img {
+  display: inline-block;
+  vertical-align: middle;
+ }
+</style>
